1237b3c36b4fce2a113dd449f70f2ce70b2e7e25
priyabrattamishra <priyabrattamishra2007@gmail.com>
2026-02-17 20:28:03 +0530

    I learnt Lower Bound Question, after struggling

---
+++ Q2.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Q2 {
+    public static void input (int [] arr){
+        Scanner sc = new Scanner (System.in);
+    for (int row = 0; row < arr.length ; row++){
+        arr [row] = sc.nextInt();
+    }
+    System.out.println("Original array");
+    System.out.println(Arrays.toString(arr));
+    }
+    public static void swap (int [] arr , int num1 , int num2) { 
+        int temp = arr [num1];
+        arr [num1] = arr [num2];
+        arr [num2] = temp;
+    }
+    public static int Binary_Search (int [] arr , int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        
+        int mid = 0;
+        boolean target_found = false;
+        int solution = 0;
+        while (start <= end) {
+            mid = start + (end - start ) / 2;
+            
+            
+                if (target <= arr [mid]) {
+                    target_found = true;
+                    solution = mid;
+                    end = mid - 1 ;
+                }
+                else {
+                    start = mid + 1;
+                } }
+        if (target_found) {
+            return solution;
+        }
+        else {
+        return arr.length;}}
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+  System.out.println("enter the size of the array"); 
+  int n = sc.nextInt();
+  int [] nums = new int [n]; 
+ input(nums); 
+ System.out.println("Enter the target element");
+ int target = sc.nextInt();
+ int res = Binary_Search(nums, target);
+ System.out.println("output: " + res);
+}}
